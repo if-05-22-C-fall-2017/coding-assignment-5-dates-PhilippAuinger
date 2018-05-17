d96957bfa3e3cd4265f2794f1eae4f30f5f9f9f2
Philipp323 <philipp-auinger@gmx.at>
2018-05-17 07:55:47 +0200

    finished - Auinger

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
+    
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern PATTERN = Pattern.compile("(^([1-2]\\d)|^(0?\\d)|^(3(0|1)))(\\.|-| )((1[0-2])|(0?\\d))(\\.|-| )((1|2)(0|9))?\\d\\d$");
+        return PATTERN.matcher(dateString);
     }
 }
